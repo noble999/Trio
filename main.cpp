9a6f8c1c35a663fa7bfd8d5930f7a677cd16cf63
noble999 <nleduc2@mail.csuchico.edu>
2019-11-13 16:57:24 -0800

    new main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // <-- ADD YOUR FUNCTION PROTOTYPE HERE
+
+void orderedValues(int &a, int &b, int &c);
 
 int main()
 {
@@ -11,9 +14,8 @@ int main()
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
 
-
   // <-- ADD YOUR FUNCTION CALL HERE
-
+  orderedValues(red,green,blue);
 
   //DO NOT CHANGE WITHIN THIS AREA...
   cout<<"Rearranged....\n";
@@ -23,3 +25,57 @@ int main()
 }
 
 // <-- ADD YOUR FUNCTION DEFINITON HERE
+void orderedValues(int &a, int &b, int &c)
+{
+ int most=0;
+ int secondMost=0;
+ int last=0;
+
+
+ if((a>b) && (a>c))
+ {
+   most=a;
+   if(b>c)
+   {
+     secondMost=b;
+     last=c;
+   }
+   else
+   {
+    secondMost=c;
+    last=b;
+   }
+ }
+ else if((b>a) && (b>c))
+ {
+  most=b;
+  if(a>c)
+  {
+   secondMost=a;
+   last=c;
+  }
+  else
+  {
+   secondMost=c;
+   last=a;
+  }
+ }
+ else if((c>a) && (c>b))
+ {
+  most=c;
+  if(a>b)
+  {
+   secondMost=a;
+   last=b;
+  }
+  else
+  {
+   secondMost=b;
+   last=a;
+  }
+ }
+ a=most;
+ b=secondMost;
+ c=last;
+ return;
+}
